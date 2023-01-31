4e1fd5a0923f93a2a49a9dc65dd69b7f13a9fb2a
SamuelePassiatore <semmisno2@protonmail.com>
2023-01-31 12:09:59 +0100

    first commit

---
+++ src/assets/data/store.js
@@ -0,0 +1,4 @@
+import {reactive} from 'vue'
+export const store = reactive ({
+
+});
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fontsource/roboto";
+
+import App from './App.vue'
+createApp(App).mount('#app')
